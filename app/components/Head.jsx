b1a2e5f09a9ba5b72736901fbafc387eb86db5b2
lorecodez <lorenzo.washington233@gmail.com>
2024-05-23 17:21:11 -0700

    working on header

--- app/components/Head.jsx
+++ app/components/Head.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+'use client'
+import React from 'react';
+import Link from 'next/link';
+import Image from 'next/image';
 
 export default function Head() {
   return (
-    <div>
-      
+    <div className='flex justify-center items-center'>
+        <picture>
+
+        </picture>
+        <menu className='flex items-center gap-5 py-3 px-6'>
+            <li>Home</li>
+            <li>About</li>
+            <li>Contact</li>
+        </menu>
     </div>
   )
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -25,7 +25,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className={`font-sans ${inter.className}`}>
         <header>
           <Suspense fallback={<p>Loading...</p>}>
             <Head/>
