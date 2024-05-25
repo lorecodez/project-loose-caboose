6f77ab3df081f4983f0f0e0171df1d7284e4ec09
lorecodez <lorenzo.washington233@gmail.com>
2024-05-24 17:22:58 -0700

    working on header

--- app/components/Head.jsx
+++ app/components/Head.jsx
@@ -2,17 +2,45 @@
 import React from 'react';
 import Link from 'next/link';
 import Image from 'next/image';
+import Logo from '@/public/loose-caboose-logo-no-bg.png';
 
 export default function Head() {
   return (
-    <div className='flex justify-center items-center'>
+    <div className='flex justify-start items-center w-full'>
         <picture>
-
+          <Image
+          src={Logo}
+          title='Logo'
+          alt='Logo'
+          width={100}
+          height={100}
+          />
         </picture>
-        <menu className='flex items-center gap-5 py-3 px-6'>
-            <li>Home</li>
-            <li>About</li>
-            <li>Contact</li>
+        <menu className='flex items-center gap-5 py-3 px-6 font-bold text-2xl'>
+            <li>
+              <Link
+              href={'/'}
+              title='Home'
+              >
+                Home
+              </Link>
+            </li>
+            <li>
+              <Link
+              href={'/about'}
+              title='About'
+              >
+                About
+              </Link>
+            </li>
+            <li>
+              <Link
+              href={'/contact'}
+              title='Contact'
+              >
+                Contact
+              </Link>
+            </li>
         </menu>
     </div>
   )
