24c9ce55b0d305daf8990cc0bea5ded38ac73862
lorecodez <lorenzo.washington233@gmail.com>
2024-05-31 16:34:11 -0700

    added schedule

--- app/components/Schedule.jsx
+++ app/components/Schedule.jsx
@@ -7,39 +7,39 @@ export default function Schedule() {
   return (
     <section className='flex flex-col items-center w-full h-full '>
         
-        <div className='px-6 py-3 flex items-center w-full h-full'>
-            <picture className='w-1/2 relative flex items-center justify-center border-4  rounded-full overflow-clip'>
+        <div className='px-6 py-3 flex items-center w-full h-full gap-5'>
+            <picture className=' relative flex items-center justify-center border-4  rounded-full overflow-clip'>
                 <Image
                 src={Owner}
                 alt='owner'
-                height={500}
-                width={500}
+                height={400}
+                width={400}
                 className='w-full object-cover shadow-inner'
                 />
             </picture>
-            <aside className='w-1/2'>
+            <aside className='w-full flex flex-col items-center justify-center'>
                 <h2 className='text-8xl font-bold self-start'>Schedule</h2>
-                <table className='w-full text-left'>
-                    <thead>
+                <table className='w-full text-left text-2xl'>
+                    <thead className='w-full'>
                         <tr>
-                            <th className='w-20'>Mon</th>
-                            <th className='w-20'>Tues</th>
-                            <th className='w-20'>Weds</th>
-                            <th className='w-20'>Thurs</th>
-                            <th className='w-20'>Fri</th>
-                            <th className='w-20'>Sat</th>
-                            <th className='w-20'>Sun</th>
+                            <th className='w-16 mr-1'>Mon</th>
+                            <th className='w-20 mr-1'>Tues</th>
+                            <th className='w-20 mr-1'>Weds</th>
+                            <th className='w-20 mr-1'>Thurs</th>
+                            <th className='w-20 mr-1'>Fri</th>
+                            <th className='w-20 mr-1'>Sat</th>
+                            <th className='w-20 mr-1'>Sun</th>
                         </tr>
                     </thead>
                     <tbody>
                         <tr>
-                            <td>Closed</td>
-                            <td>10am-6pm</td>
-                            <td>10am-6pm</td>
-                            <td>10am-6pm</td>
-                            <td>10am-6pm</td>
-                            <td>10am-6pm</td>
-                            <td>12pm-5pm</td>
+                            <td className='w-20 mr-1'>Closed</td>
+                            <td className='w-20 mr-1'>10am-6pm</td>
+                            <td className='w-20 mr-1'>10am-6pm</td>
+                            <td className='w-20 mr-1'>10am-6pm</td>
+                            <td className='w-20 mr-1'>10am-6pm</td>
+                            <td className='w-20 mr-1'>10am-6pm</td>
+                            <td className='w-20 mr-1'>12pm-5pm</td>
                         </tr>
                     </tbody>
                 </table>
